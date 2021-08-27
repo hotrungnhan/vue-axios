9f16cf14d04355f278fbe7b737ab4ee6a7e06722
KudouSterain <hotrungnhan29@gmail.com>
2021-08-27 18:18:53 +0700

    add log

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@
  * @param axios
  */
 /* vue 3 and up*/
-export default {
+let plugin = {
   install: (app, axios) => {
+    if (!axios) {
+      console.log('axios isn`t imported')
+    }
     if (app.version && app.version.split('.')[0] >= '3' && app.provide) {
       app.config.globalProperties.$axios = axios
       app.$axios = axios
@@ -16,8 +19,14 @@ export default {
     }
   },
 }
-//vue 2 web
-if (  Vue &&  Vue.version &&  Vue.version.split('.')[0] <= '2' &&  Vue &&  window.axios &&  window.Vue.use) {
-  Vue.$axios = axios
-  Vue.prototype.$axios = axios
+export default plugin
+if (
+  window.Vue &&
+  Vue.version &&
+  Vue.version.split('.')[0] <= '2' &&
+  window.Vue.use
+) {
+  if (axios) {
+    Vue.use(plugin, axios)
+  }
 }
